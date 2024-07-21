7a9a479fde4f85c0b81087a3c5b274c598c946f8
Amit <3540115+amitdeshmukh@users.noreply.github.com>
2024-07-21 10:34:44 +0530

    feat: :sparkles: add support for Groq models

--- src/config/index.ts
+++ src/config/index.ts
@@ -18,6 +18,7 @@ const ANTHROPIC_API_KEY: string | undefined = process.env.ANTHROPIC_API_KEY;
 const OPENAI_API_KEY: string = process.env.OPENAI_API_KEY || '';
 const COHERE_API_KEY: string = process.env.COHERE_API_KEY || '';
 const GEMINI_API_KEY: string = process.env.GEMINI_API_KEY || '';
+const GROQ_API_KEY: string = process.env.GROQ_API_KEY || '';
 
 interface ProviderApiKeys {
   [key: string]: string | undefined;
@@ -26,6 +27,7 @@ interface ProviderApiKeys {
 const PROVIDER_API_KEYS: ProviderApiKeys = {
   COHERE_API_KEY,
   GEMINI_API_KEY,
+  GROQ_API_KEY,
   OPENAI_API_KEY,
   ANTHROPIC_API_KEY,
 };
